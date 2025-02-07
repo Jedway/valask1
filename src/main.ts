2e170153433dcc8aa8be5e6a833d5bd8200cd869
JED <jandedjedi72@gmail.com>
2025-02-07 04:30:29 +0100

    pwa upgrade
    upgraded to pwa

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import './app.css'
-import App from './App.svelte'
+import './app.css';
+import App from './App.svelte';
 
 const app = new App({
   target: document.getElementById('app')!,
-})
+});
 
-export default app
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, (err) => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+export default app;
